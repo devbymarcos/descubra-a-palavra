e8c38380193995c72c28d7dc89cc196d6ef2e7a3
devbymarcos <marcoslopes.dev@gmail.com>
2021-12-27 10:19:13 -0300

    carregando div para o alerta

--- src/modules/actionLetter.js
+++
@@ -1,5 +0,0 @@
-export default function actionLetter() {
-  const inletter = document.querySelector("#inLetter");
-  const btnThrow = document.querySelector("#btnThrow");
-  const viewTip = document.querySelector("#viewTip");
-}
